022763b95bb45cf5ab2bc131c45282951387798d
Nilankar <98743539+nilankar19@users.noreply.github.com>
2024-04-18 21:39:04 +0530

    Update PortfolioIntro.js

--- components/intro/PortfolioIntro.js
+++ components/intro/PortfolioIntro.js
@@ -8,7 +8,7 @@ export default function PortfolioIntro({ children }) {
       {/* <Image src="/assets/img/.jpg" alt="banner" width={400} height={40} /> */}
       <div className="  banner flex flex-col  justify-center text-center   rounded-lg pt-14 pb-14  text-white">
         <div className="flex flex-row justify-center  w-full  relative intro-div">
-          <h1 className="relative text-4xl ">Hi there,Im fullStack developer </h1>
+          <h1 className="relative text-4xl ">Hello! 👋 I'm Nilankar, an Innovative Cloud Solutions Architect ☁️ </h1>
           
         </div>
         {children}
